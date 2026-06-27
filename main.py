db6c143b894447e981e9d04de9a54ff554978975
Krishna Prasad Guntur <gvns.krishna1@gmail.com>
2026-06-27 20:44:46 +0530

    Added basic chain

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+
 from dotenv import load_dotenv
 import os
+from prompts.summaryTemplate import SummaryTemplate
+
 load_dotenv()
 
 
 def main():
+
+    entity = input("Give an entity name: ")
     
-    print("Hello from langchain-project!")
+    summary_prompt_template = PromptTemplate(
+        input_variables=['entity'],
+        template=SummaryTemplate
+    )
+    llm = ChatOllama(temperature=0, model="granite3.3:latest")
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"entity": entity})
+    print(response.content)
 
 
 if __name__ == "__main__":
---
+++ prompts/summaryTemplate.py
@@ -0,0 +1,8 @@
+SummaryTemplate = '''
+    You are an omnipotent of the entire universe.
+
+    With all the information you have on {entity}, I want you to create:
+
+    1. A short summary.
+    2. Two interesting facts about them.       
+'''
